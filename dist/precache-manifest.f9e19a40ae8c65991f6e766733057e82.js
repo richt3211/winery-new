1353c7f6065b1afc74c5cd886ee3d06912cd750f
Richard Timpson <richardtimpson80@gmail.com>
2018-10-10 08:30:22 -0600

    stuff

---
+++ dist/precache-manifest.f9e19a40ae8c65991f6e766733057e82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "1cada1c261e127ed7a06",
+    "url": "/js/chunk-vendors.e858c3d8.js"
+  },
+  {
+    "revision": "2ecd92c96baa61debd33",
+    "url": "/js/app.2272d368.js"
+  },
+  {
+    "revision": "5ec49e6dccb2ca4932e9",
+    "url": "/js/about.07b90307.js"
+  },
+  {
+    "revision": "c7be79c981a81a797ca84e26c4cae8a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "2ecd92c96baa61debd33",
+    "url": "/css/app.115d5e2b.css"
+  }
+];
